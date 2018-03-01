07d9e6fa09910bbe363fd70fc9e46827cd78b026
GattyGamaun <gattygamaun@gmail.com>
2018-03-01 15:34:59 +0300

    additional-5/future: the first solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+        stack.push(str[i]);
+    }
+
+    if (stack[0] === bracketsConfig[0][0] && stack[1] === bracketsConfig[0][1]) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+
+
