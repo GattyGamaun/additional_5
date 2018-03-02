c36cacc5c8e384ef838f064137384e3447a2c7f6
GattyGamaun <gattygamaun@gmail.com>
2018-03-02 12:50:51 +0300

    additional_5/feature: the third solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
 
     let length = str.length;
-
-    if (str[0] === bracketsConfig[0][0] && (str[length - 1] === bracketsConfig[0][0 + 1] || str[0 + 1] === bracketsConfig[0][0 + 1])) {
+    if ((str[0] === bracketsConfig[0][0] && str[length - 1] === bracketsConfig[0][1])) {
         return true;
     } else {
         return false;
     }
 }
+
+
+
 
 
